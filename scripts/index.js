9722d5b3bc5c9409a95f18054e10cc0a9c876cc8
TessaViergever <tessa.viergever@hva.nl>
2022-12-08 08:21:31 +0100

    html/css/js -> pop up + blurry works

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
 //Stap 1: JS koppelen aan CSS/HTML //
 
 let SearchButton = document.querySelector ('#search-button') //(SELECT)//
-let PopUpSearch = document.querySelector ('.pop-up-search')
-let PopUpBlurryBackground = document.querySelector ('.pop-up-blurry-background')
+let ContainerSearchPopUp = document.querySelector ('.container-search-pop-up')
 
 
 //Stap 2: event selecteren (IF this...) //
@@ -11,11 +10,11 @@ SearchButton.addEventListener ('click', showSearch) //(EVENT)//
 
 //Stap 3: functie toewijzen (...THEN that) //
 function showSearch() {
-    PopUpSearch.classList.toggle('display-pop-up-search') //(FUNCTION)//
+    ContainerSearchPopUp.classList.toggle('display-search-container') //(FUNCTION)//
 }
 
 
-function showSearch() {
-    PopUpSearch.classList.toggle('.display-blurry-background') 
-}
+// function showSearch() {
+//     PopUpSearch.classList.toggle('.display-blurry-background') 
+// }
 
