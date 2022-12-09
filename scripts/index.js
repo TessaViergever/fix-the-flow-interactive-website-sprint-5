a481ed2ea5c9f6f50367db10739fdc0b6fb08ede
TessaViergever <tessa.viergever@hva.nl>
2022-12-09 10:02:54 +0100

    html/css/js -> dialog poging

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,13 @@
-//Stap 1: JS koppelen aan CSS/HTML //
+// -------- OPEN AND CLOSE SEARCH POP-UP WINDOW ------------ //
 
+//Stap 1: JS koppelen aan CSS/HTML //
 let SearchButton = document.querySelector ('#search-button') //(SELECT)//
 let ContainerSearchPopUp = document.querySelector ('.container-search-pop-up')
 let BlurryBackground = document.querySelector ('.pop-up-blurry-background')
 
-
-
 //Stap 2: event selecteren (IF this...) //
 SearchButton.addEventListener ('click', showSearch) //(EVENT)//
 BlurryBackground.addEventListener ('click', close)
-
-
 
 //Stap 3: functie toewijzen (...THEN that) //
 function showSearch() {
@@ -21,5 +18,15 @@ function close() {
     ContainerSearchPopUp.classList.toggle ('display-search-container') //closes when click on blurrybackground//
 }
 
+// --------   DIALOG WINDOW OPEN/CLOSE  ------------ //
 
+// let SearchButton = document.querySelector('#search-button')
+// let Dialog = document.querySelector('.search-dialog')
+// // let Backdrop = document.querySelector
+
+// SearchButton.addEventListener('click', showSearch)
+
+// function showSearch() {
+//     Dialog.classList.toggle('.dialog-active')
+// }
 
